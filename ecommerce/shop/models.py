85fb1e561a4dabd1db2b700382f830c9b2edcc90
Tzekov <stanislav.tzekov@dxc.com>
2024-11-27 15:25:30 +0200

    Register Product model in database

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -8,4 +8,6 @@ class Product(models.Model):
     category = models.CharField(max_length=255)
     description = models.TextField()
     image = models.CharField(max_length=500)
-    
+
+    def __str__(self):
+        return self.title
