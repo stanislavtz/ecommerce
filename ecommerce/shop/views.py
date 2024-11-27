c5996415bf31d529eddd0c8f7e65ab671de41d6a
Tzekov <stanislav.tzekov@dxc.com>
2024-11-27 15:13:47 +0200

    Add index view

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Product
+from django.shortcuts import get_list_or_404
+
 
 # Create your views here.
+def index(request):
+    products = get_list_or_404(Product)
+    
+    return render(request, "shop/index.html", {"products": products})
