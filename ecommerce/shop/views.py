030474fbae200478c71aeaf4decacd4ef6013014
Tzekov <stanislav.tzekov@dxc.com>
2024-11-28 08:32:49 +0200

    Add search functionality

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -7,4 +7,8 @@ from django.shortcuts import get_list_or_404
 def index(request):
     products = Product.objects.all()
     product_title = request.GET.get("item_name")
+
+    if product_title != "" and product_title is not None:
+        products = products.filter(title__icontains=product_title)
+        
     return render(request, "shop/index.html", {"products": products})
