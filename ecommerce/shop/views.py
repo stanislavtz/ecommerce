3c8bad4ef8ae549e35228d28269e877a70869e54
Tzekov <stanislav.tzekov@dxc.com>
2024-11-28 08:29:08 +0200

    Update index html template

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -6,5 +6,5 @@ from django.shortcuts import get_list_or_404
 # Create your views here.
 def index(request):
     products = Product.objects.all()
-    
+    product_title = request.GET.get("item_name")
     return render(request, "shop/index.html", {"products": products})
