080d20b515f5be2c0e739b7ac81e0db6a8b9646d
Tzekov <stanislav.tzekov@dxc.com>
2024-12-04 15:22:52 +0200

    Create order model

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -11,3 +11,17 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Order(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    address = models.TextField(max_length=500)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=10)
+    cart_items = models.CharField(max_length=1000)
+    amount_total = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name + "'s order."
