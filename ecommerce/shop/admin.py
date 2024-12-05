798414493035ccbf38eb94f36d2a48d4385e2082
Tzekov <stanislav.tzekov@dxc.com>
2024-12-05 09:06:09 +0200

    Customise DB admin panel

--- ecommerce/shop/admin.py
+++ ecommerce/shop/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from .models import Product, Order
 
+admin.site.site_header = "E-Commerce Administration"
+admin.site.site_title = "E-Commerce"
+admin.site.index_title = "Manage E-Store"
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("title", "price", "discount_price", "category",)
+    list_editable = ("price", "discount_price", "category",)
+    list_filter = ("title", "category",)
+    search_fields = ("title", "category", "price")
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "address", "amount_total",)
+    list_filter = ("name", "city", "amount_total",)
+    search_fields = ("name", "city", "address",)
+
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Order)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
